09507fea985721fa8c2d1e98202ec3347cb185af
wujinzhe <1037680926@qq.com>
2017-01-07 14:02:21 +0800

    we chat   install

---
+++ routes/wechat.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var wechat = require('wechat');
+
+
+var config={
+    token:'wxexpress',
+    appid:'wxeb76aa92a1398e2d',
+    appsecret:'677b3785d5ccdb0a629905223dbb225d',
+    encodingAESKey:''
+}
+
+router.use(express.query());
+
+router.use('/',wechat(config,function(req, res, next){
+    console.log(req.weixin);
+    var message = req.weixin;
+    if(message.Content == '1'){
+        res.reply('hhe');
+    }
+}));
+
+module.exports = router;
